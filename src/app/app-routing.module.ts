b9272306000083287df3d9bc1500df3852130c1d
yozue_547@hotmail.com <yozue_5470@hotmail.com>
2019-09-20 11:58:32 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PickingComponent } from './components/picking/picking.component';
+import { PreruteoComponent } from './components/preruteo/preruteo.component';
 
 
 
 
 const routes: Routes = [
   {path: 'picking', component: PickingComponent},
+  {path: 'preruteo', component: PreruteoComponent},
   {path: '**', component: PickingComponent}
 ];
 
